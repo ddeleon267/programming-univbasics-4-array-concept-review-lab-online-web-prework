f6028027287cce33d657698716fe858cf0536b23
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-22 08:39:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,7 +15,10 @@ def find_max_value(array)
   i = 0
   
   while i < array.length
-   max = array[i] > max ? array[i] : max
+    max = array[i] > max ? array[i] : max
+    i += 1
+  end
+  max
 end
 
 def find_min_value(array)
